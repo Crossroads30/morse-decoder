6c7c92f15baa2403b7be1e3834fd694a734969f3
Crossroads30 <Crossroads30@mail.ru>
2023-02-21 21:02:03 +0300

    feat: add code with tests passing

--- src/index.js
+++ src/index.js
@@ -38,8 +38,64 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+   // expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"
+   expr = expr.split('');
+   const arr = expr;
+   const res = [];
+      while (arr.length > 0) {
+         const chunk = arr.splice(0, 10);
+         res.push(chunk);
+      };
+
+   let res1 = []
+   res.forEach(el=>{
+      for (let i = 0; i < el.length; i += 2) {
+         let cut = el.slice(i, i + 2);
+         res1.push(cut);
+      };
+   });
+
+   let res2 = [];
+      for ( i=0; i<res1.length;i++){
+         res2.push(res1[i].join(''))
+      };
+
+   let res3 = [];
+   while (res2.length > 0) {
+      let cut = res2.splice(0, 5);
+      res3.push(cut);
+   };
+
+      let morseCode = []
+      res3.forEach(el=>{
+         for (let i = 0; i < el.length; i++) {
+            if ( el[i] == '10'){
+               el[i] = '.'
+            } else if ( el[i] == '11' ) {
+               el[i] = '-'
+            } else if ( el[i] == '**' ) {
+               el[i] = ' '
+            } else 
+               el[i] = ''
+         }
+      })
+   
+      res3.forEach(el=>{
+         morseCode.push(el.join('').replace(/ /,'').replace(/ /,'').replace(/ /,'').replace(/ /,''))
+      })
+   
+      let entries = Object.entries(MORSE_TABLE);
+      console.log(entries);
+   
+      entries.forEach(el=>{
+         for (let i = 0; i < morseCode.length; i +=1){
+            if ( el[0] == morseCode[i]){
+               morseCode[i]= el[1]
+            }
+         }
+      })
+      return morseCode.join('')
+   };
 
 module.exports = {
     decode
